12c821baf228adf8fa16acf44cd743ec849b7bcd
OfekShaked <ofekshaked1999@gmail.com>
2021-12-19 00:06:33 +0200

    email sending

--- TestingSite.Backend/BLL/controllers/email_controller.js
+++ TestingSite.Backend/BLL/controllers/email_controller.js
@@ -1,22 +1,51 @@
 const nodemailer = require("nodemailer");
 
-class EmailService{
-    constructor(){
-
+class EmailService {
+  constructor() {
+    this.createTransporter();
+  }
+  createTransporter = () => {
+    let mailConfig;
+    if (process.env.NODE_ENV === "production") {
+      // all emails are delivered to destination
+      mailConfig = {
+        service: "gmail",
+        auth: {
+          user: process.env.HOST_EMAIL,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+      };
+    } else {
+      // all emails are catched by ethereal.email
+      mailConfig = {
+        host: "smtp.ethereal.email",
+        port: 587,
+        auth: {
+          user: "odessa.jakubowski59@ethereal.email",
+          pass: "Q3bqcjzsyMC98JFGTG",
+        },
+      };
     }
-    createTransporter = () =>{
-        let transporterConfig = {
-            host: 'smtp.sendgrid.net',
-            port: 587,
-            auth: {
-                user: 'real.user',
-                pass: 'verysecret'
-            }
-        };
-        this.transporter = nodemailer.createTransport(transporterConfig);
-    }
-    
+    this.transporter = nodemailer.createTransport(mailConfig);
+  };
 
+  send_email = (sender, reciever, subject, message) => {
+    var mailOptions = {
+      from: process.env.NODE_ENV === "production"?sender:"odessa.jakubowski59@ethereal.email",
+      to: reciever,
+      subject: subject,
+      text: message,
+    };
+
+    this.transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+        console.log("Preview URL: " + nodemailer.getTestMessageUrl(info));
+      }
+    });
+  };
 }
 
 module.exports = new EmailService();
--- TestingSite.Backend/BLL/controllers/test_taken_controller.js
+++ TestingSite.Backend/BLL/controllers/test_taken_controller.js
@@ -2,7 +2,8 @@ const TestTakenModel = require("../../models/test_taken");
 const user_controller = require("./user_controller");
 const test_controller = require("./test_controller");
 const test_taken_question_controller = require("./test_taken_questions_controller");
-const user_test_report_controller = require('./user_report_controller');
+const user_test_report_controller = require("./user_report_controller");
+const email_controller = require("./email_controller");
 const logger = require("../../logger");
 
 class TestTakenService {
@@ -12,25 +13,17 @@ class TestTakenService {
     const validatedModel = model.validateSync();
     if (!!validatedModel) return null;
     try {
-      const test_taken_added = await TestTakenModel.create(model);
-      await TestTakenModel.populate(test_taken_added, "test_id");
-      await TestTakenModel.populate(test_taken_added, "user");
-      await TestTakenModel.populate(test_taken_added, {
-        path: "test_questions",
-        model: "TestTakenQuestion",
-        populate: {
-          path: "question_id",
-          model: "Question",
-          populate: { path: "optional_answers", model: "Answer" },
-        },
-      });
-      await TestTakenModel.populate(test_taken_added,{path:"test_questions.answers_chosen", model: "Answer"});
+      let test_taken_added = await TestTakenModel.create(model);
+      test_taken_added = await populate_test_taken(test_taken_added);
       test_taken_added.grade = await calculate_grade(test_taken_added);
       await test_taken_added.save();
       await test_controller.set_test_active(test_taken_added._id);
-      if(test_taken_added.test_id.is_answer_shown){
-        return user_test_report_controller.get_user_test_report(test_taken_added._id);
-      }else{
+      send_mail(test_taken_added);
+      if (test_taken_added.test_id.is_answer_shown) {
+        return user_test_report_controller.get_user_test_report(
+          test_taken_added._id
+        );
+      } else {
         return {
           grade: test_taken_added.grade,
         };
@@ -41,16 +34,16 @@ class TestTakenService {
   };
 }
 
-
 const add_user_and_questions = async (test_taken) => {
   const user_added = await user_controller.add_user(test_taken.user);
   if (user_added == null) return null;
   else test_taken.user = user_added;
   let test_taken_questions_added = [];
   for (let index = 0; index < test_taken.test_questions.length; index++) {
-      const ques = test_taken.test_questions[index];
-      const test_taken_question = await test_taken_question_controller.add_test_taken_question(ques);
-      test_taken_questions_added.push(test_taken_question);
+    const ques = test_taken.test_questions[index];
+    const test_taken_question =
+      await test_taken_question_controller.add_test_taken_question(ques);
+    test_taken_questions_added.push(test_taken_question);
   }
   if (test_taken_questions_added.includes(null)) return null;
   test_taken.test_questions = test_taken_questions_added;
@@ -60,24 +53,79 @@ const add_user_and_questions = async (test_taken) => {
 const calculate_grade = async (test_taken) => {
   const score_for_question = 100 / test_taken.test_questions.length;
   let grade = 0;
-  for (let i = 0; i < test_taken.test_questions.length; i++) { //loop questions
+  for (let i = 0; i < test_taken.test_questions.length; i++) {
+    //loop questions
     const question = test_taken.test_questions[i];
     let num_of_correct_questions = 0;
     question.question_id.optional_answers.forEach((ans) => {
       if (ans.is_correct) num_of_correct_questions++;
     });
     let is_skip = false;
-    for (let j = 0; j < question.answers_chosen.length; j++) { //loop answers
+    for (let j = 0; j < question.answers_chosen.length; j++) {
+      //loop answers
       const answer = question.answers_chosen[j];
-      if (!answer.is_correct) { //if answer is incorrect skip
-        is_skip=true;
-        j=question.answers_chosen.length;
+      if (!answer.is_correct) {
+        //if answer is incorrect skip
+        is_skip = true;
+        j = question.answers_chosen.length;
       }
     }
-    if(is_skip) continue;
-    if (question.answers_chosen.length === num_of_correct_questions) //add score if all correct
+    if (is_skip) continue;
+    if (question.answers_chosen.length === num_of_correct_questions)
+      //add score if all correct
       grade += score_for_question;
   }
   return grade;
+};
+
+const populate_test_taken = async (test) => {
+  await TestTakenModel.populate(test, "test_id");
+  await TestTakenModel.populate(test, "user");
+  await TestTakenModel.populate(test, {
+    path: "test_questions",
+    model: "TestTakenQuestion",
+    populate: {
+      path: "question_id",
+      model: "Question",
+      populate: { path: "optional_answers", model: "Answer" },
+    },
+  });
+  await TestTakenModel.populate(test, {
+    path: "test_questions.answers_chosen",
+    model: "Answer",
+  });
+  return test;
+};
+
+const send_mail = (test) => {
+  //send mail after grade recieved
+  let isPassed = test.grade >= test.test_id.passing_grade;
+  let subject = isPassed
+    ? "Good job! Passed the test"
+    : "Sorry you have not passed the test";
+  let message = isPassed
+    ? test.test_id.email_success_content
+    : test.test_id.email_failed_conent;
+  message = replace_message_templates(test, message);
+  email_controller.send_email(
+    test.test_id.tester_email,
+    test.user.email,
+    subject,
+    message
+  );
+};
+
+const replace_message_templates = (test, message) => {
+  //replace Predefined templates with the real data
+  let updatedMessage = message.replace("@TestName@", test.test_id.name);
+  updatedMessage = message.replace("@FirstName@", test.user.name.first);
+  updatedMessage = message.replace("@LastName@", test.user.name.last);
+  updatedMessage = message.replace("@Date@", test.createdAt);
+  updatedMessage = message.replace("@Grade@", test.grade);
+  updatedMessage = message.replace(
+    "@CertificateURL@",
+    test.test_id.diploma_url
+  );
+  return updatedMessage;
 };
 module.exports = new TestTakenService();
